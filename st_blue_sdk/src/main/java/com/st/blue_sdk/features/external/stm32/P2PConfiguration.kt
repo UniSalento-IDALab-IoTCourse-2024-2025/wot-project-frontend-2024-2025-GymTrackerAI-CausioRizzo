4d3599753b0ee2055b57effcfd8f1f3804147184
Davide Rizzo <101137706+davide09@users.noreply.github.com>
2025-06-19 14:50:28 +0200

    Add files via upload

---
+++ st_blue_sdk/src/main/java/com/st/blue_sdk/features/external/stm32/P2PConfiguration.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2022(-0001) STMicroelectronics.
+ * All rights reserved.
+ * This software is licensed under terms that can be found in the LICENSE file in
+ * the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ */
+package com.st.blue_sdk.features.external.stm32
+
+import com.st.blue_sdk.board_catalog.models.BoardFirmware
+import com.st.blue_sdk.board_catalog.models.BoardFotaType
+import com.st.blue_sdk.models.Boards
+
+// TODO: move this item in P2P demo package
+object P2PConfiguration {
+    private const val WB_ROUTER_NODE_ID = 0x85
+    sealed class DeviceId(val id: Byte) {
+        object Device1 : DeviceId(id = 0x01)
+        object Device2 : DeviceId(id = 0x02)
+        object Device3 : DeviceId(id = 0x03)
+        object Device4 : DeviceId(id = 0x04)
+        object Device5 : DeviceId(id = 0x05)
+        object Device6 : DeviceId(id = 0x06)
+        object AllDevices : DeviceId(0x00)
+    }
+
+    fun getDeviceIdById(id: Byte): DeviceId? {
+        return when (id) {
+            0x01.toByte() -> DeviceId.Device1
+            0x02.toByte()-> DeviceId.Device2
+            0x03.toByte() -> DeviceId.Device3
+            0x04.toByte() -> DeviceId.Device4
+            0x05.toByte() -> DeviceId.Device5
+            0x06.toByte() -> DeviceId.Device6
+            0x00.toByte() ->DeviceId.AllDevices
+            else -> null
+        }
+    }
+
+    //This must be removed
+//    fun getDeviceIdByBoardId(boardId: Int): DeviceId? {
+//        return when (boardId) {
+//            0x83 -> DeviceId.Device1
+//            0x84 -> DeviceId.Device2
+//            0x87 -> DeviceId.Device3
+//            0x88 -> DeviceId.Device4
+//            0x89 -> DeviceId.Device5
+//            0x8A -> DeviceId.Device6
+//            else -> null
+//        } //   }
+
+    fun getDeviceIdByBoardId(boardId: Int, sdkVersion: Int): DeviceId? {
+        return if(sdkVersion==1) {
+            when (boardId) {
+                0x83 -> DeviceId.Device1
+                0x84 -> DeviceId.Device2
+                0x87 -> DeviceId.Device3
+                0x88 -> DeviceId.Device4
+                0x89 -> DeviceId.Device5
+                0x8A -> DeviceId.Device6
+                else -> null
+            }
+        } else {
+            if(boardId==0x83) {
+                DeviceId.Device1
+            } else {
+                null
+            }
+        }
+    }
+
+    fun isValidDeviceNode(
+        boardId: Int,
+        sdkVersion: Int,
+        protocolVersion: Short,
+        boardFirmware: BoardFirmware?
+    ): Boolean {
+
+        val boardModel = Boards.getModelFromIdentifier(boardId,sdkVersion)
+        val familyModel = Boards.getFamilyFromModel(boardModel)
+        if (familyModel == Boards.Family.WB_FAMILY) {
+            val deviceId = runCatching { getDeviceIdByBoardId(boardId,protocolVersion.toInt()) }.getOrNull()
+            return deviceId != null
+        }
+
+        //if (protocolVersion.toInt() == PROTOCOL_V2) {
+        if(familyModel== Boards.Family.WBA_FAMILY) {
+            boardFirmware?.let {
+                return it.fota.type == BoardFotaType.WB_MODE
+            }
+        }
+
+        return false
+    }
+
+    fun isValidDeviceRouter(boardId: Int,sdkVersion: Int): Boolean {
+        val boardModel = Boards.getModelFromIdentifier(boardId,sdkVersion)
+        val familyModel = Boards.getFamilyFromModel(boardModel)
+        return familyModel == Boards.Family.WB_FAMILY && boardId == WB_ROUTER_NODE_ID
+    }
+}
---
+++ st_blue_sdk/src/main/java/com/st/blue_sdk/features/switchfeature/SwitchFeature.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2022(-0001) STMicroelectronics.
+ * All rights reserved.
+ * This software is licensed under terms that can be found in the LICENSE file in
+ * the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ */
+package com.st.blue_sdk.features.switchfeature
+
+import com.st.blue_sdk.features.*
+import com.st.blue_sdk.features.switchfeature.request.SwitchOff
+import com.st.blue_sdk.features.switchfeature.request.SwitchOn
+import com.st.blue_sdk.features.switchfeature.response.SwitchOnOffResponse
+import com.st.blue_sdk.utils.NumberConversion
+
+class SwitchFeature(
+    name: String = NAME,
+    type: Type = Type.STANDARD,
+    isEnabled: Boolean,
+    identifier: Int
+) : Feature<SwitchFeatureInfo>(
+    name = name,
+    type = type,
+    isEnabled = isEnabled,
+    identifier = identifier
+) {
+
+    companion object {
+        const val NAME = "Switch"
+        const val COMMAND_SWITCH_ON: Byte = 0x01
+        const val COMMAND_SWITCH_OFF: Byte = 0x00
+
+        fun getStatusType(status: Short) = when (status.toInt()) {
+            0x00 -> SwitchStatusType.Off
+            0x01 -> SwitchStatusType.On
+            else -> SwitchStatusType.Error
+        }
+
+        fun getStatusTypeCode(status: SwitchStatusType) = when (status) {
+            SwitchStatusType.Off -> 0x00
+            SwitchStatusType.On -> 0x01
+            SwitchStatusType.Error -> 0xFF
+        }
+    }
+
+    override fun extractData(
+        timeStamp: Long,
+        data: ByteArray,
+        dataOffset: Int
+    ): FeatureUpdate<SwitchFeatureInfo> {
+        require(data.size - dataOffset > 0) { "There are no bytes available to read for $name feature" }
+
+        val switch = SwitchFeatureInfo(
+            status = FeatureField(
+                value = getStatusType(NumberConversion.byteToUInt8(data, dataOffset)),
+                name = "Status"
+            )
+        )
+        return FeatureUpdate(
+            featureName = name,
+            timeStamp = timeStamp, rawData = data, readByte = 1, data = switch
+        )
+    }
+
+
+    override fun packCommandData(
+        featureBit: Int?,
+        command: FeatureCommand
+    ): ByteArray? {// TODO: To Be Checked
+        return when (command) {
+            is SwitchOn -> packCommandRequest(
+                featureBit,
+                COMMAND_SWITCH_ON,
+                byteArrayOf()
+            )
+
+            is SwitchOff -> packCommandRequest(
+                featureBit,
+                COMMAND_SWITCH_OFF,
+                byteArrayOf()
+            )
+
+            else -> null
+        }
+    }
+
+    override fun parseCommandResponse(data: ByteArray): FeatureResponse? {
+        return unpackCommandResponse(data)?.let {
+            if (mask != it.featureMask) return null
+
+            val status = getStatusType(NumberConversion.byteToUInt8(it.payload))
+            SwitchOnOffResponse(
+                feature = this,
+                commandId = it.commandId,
+                status = status
+            )
+        }
+    }
+}
